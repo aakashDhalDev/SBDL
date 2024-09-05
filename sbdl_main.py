5ac05be0d794ed368959da3698692db54a216e60
Aakash Dhal <134286523+aakashDhalDev@users.noreply.github.com>
2024-09-04 18:05:57 -0700

    added unit testing

--- sbdl_main.py
+++ sbdl_main.py
@@ -1,5 +1,7 @@
 import sys
 import uuid
+from multiprocessing.connection import address_type
+
 from pyspark.sql.functions import struct, col, to_json
 from lib import Utils, ConfigLoader, DataLoader, Transformations
 from lib.logger import Log4j
@@ -12,9 +14,44 @@ if __name__ == '__main__':
 
     job_run_env = sys.argv[1].upper()
     load_date = sys.argv[2]
+    job_run_id = "SBDL-" + str(uuid.uuid4())
 
+    print("Initializing SBDL Job in "+ job_run_env +" Job ID: "+ job_run_id)
+    conf = ConfigLoader.get_config(job_run_env)
+    enable_hive = True if conf["enable.hive"] == "true" else False
+    hive_db = conf["hive.database"]
+
+    print("Creating Spark Session")
     spark = Utils.get_spark_session(job_run_env)
+
     logger = Log4j(spark)
+
+    logger.info("Reading SBDL Account DF")
+    accounts_df = DataLoader.read_accounts(spark, job_run_env, enable_hive, hive_db)
+    contract_df = Transformations.get_contract(accounts_df)
+
+    logger.info("Reading SBDL Party DF")
+    parties_df = DataLoader.read_accounts(spark, job_run_env, enable_hive, hive_db)
+    relations_df = Transformations.get_contract(accounts_df)
+
+    logger.info("Reading SBDL Address DF")
+    address_df = DataLoader.read_accounts(spark, job_run_env, enable_hive, hive_db)
+    relations_address_df = Transformations.get_contract(accounts_df)
+
+    logger.info("Join Party Relations and Address")
+    party_address_df = Transformations.join_party_address(relations_df, relations_address_df)
+
+    logger.info("Join Account and Parties")
+    data_df = Transformations.join_party_address(contract_df, party_address_df)
+
+    logger.info("Apply Header & Create Event")
+    final_df = Transformations.apply_header(spark, data_df)
+
+    logger.info("Preparing to send data to Kafka")
+    kafka_kv_df = final_df.select(col("payload.contractIdentifier.newValue").alias("key"),
+                                  to_json(struct("*")).alias("value"))
+
+
 
     logger.info("Finished creating Spark Session")
     logger.info("One Last Message")
--- test_pytest_sbdl.py
+++ test_pytest_sbdl.py
@@ -1,15 +1,100 @@
 import pytest
-
+from lib import DataLoader,Transformations
 from lib.Utils import get_spark_session
+from lib.ConfigLoader import get_config
+from pyspark.sql.types import *
+from datetime import datetime, date
 
-
+#this is to mark that this function is not for unit testing, it should be run just once
+#to set the reusable variables, database connections, or sessions to run throughout the program
 @pytest.fixture(scope='session')
 def spark():
     return get_spark_session("LOCAL")
 
+@pytest.fixture(scope='session')
+def expected_party_rows():
+    return [Row(load_date=date(2022, 8, 2), account_id='6982391060',
+                party_id='9823462810', relation_type='F-N', relation_start_date=datetime(2019, 7, 29, 6, 21, 32)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391061', party_id='9823462811', relation_type='F-N',
+                relation_start_date=datetime(2018, 8, 31, 5, 27, 22)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391062', party_id='9823462812', relation_type='F-N',
+                relation_start_date=datetime(2018, 8, 25, 15, 50, 29)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391063', party_id='9823462813', relation_type='F-N',
+                relation_start_date=datetime(2018, 5, 11, 7, 23, 28)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391064', party_id='9823462814', relation_type='F-N',
+                relation_start_date=datetime(2019, 6, 6, 14, 18, 12)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391065', party_id='9823462815', relation_type='F-N',
+                relation_start_date=datetime(2019, 5, 4, 5, 12, 37)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391066', party_id='9823462816', relation_type='F-N',
+                relation_start_date=datetime(2019, 5, 15, 10, 39, 29)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391067', party_id='9823462817', relation_type='F-N',
+                relation_start_date=datetime(2018, 5, 16, 9, 53, 4)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391068', party_id='9823462818', relation_type='F-N',
+                relation_start_date=datetime(2017, 11, 27, 1, 20, 12)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391067', party_id='9823462820', relation_type='F-S',
+                relation_start_date=datetime(2017, 11, 20, 14, 18, 5)),
+            Row(load_date=date(2022, 8, 2), account_id='6982391067', party_id='9823462821', relation_type='F-S',
+                relation_start_date=datetime(2018, 7, 19, 18, 56, 57))]
 
-def test_blank_test(spark):
-    print(spark.version)
-    assert spark.version == "3.5.0"
+@pytest.fixture(scope='session')
+def expected_contract_df(spark):
+    schema = StructType([StructField('account_id', StringType()),
+                         StructField('contractIdentifier',
+                                     StructType([StructField('operation', StringType()),
+                                                 StructField('newValue', StringType()),
+                                                 StructField('oldValue', NullType())])),
+                         StructField('sourceSystemIdentifier',
+                                     StructType([StructField('operation', StringType()),
+                                                 StructField('newValue', StringType()),
+                                                 StructField('oldValue', NullType())])),
+                         StructField('contactStartDateTime',
+                                     StructType([StructField('operation', StringType()),
+                                                 StructField('newValue', TimestampType()),
+                                                 StructField('oldValue', NullType())])),
+                         StructField('contractTitle',
+                                     StructType([StructField('operation', StringType()),
+                                                 StructField('newValue',
+                                                             ArrayType(StructType(
+                                                                 [StructField('contractTitleLineType', StringType()),
+                                                                  StructField('contractTitleLine', StringType())]))),
+                                                 StructField('oldValue', NullType())])),
+                         StructField('taxIdentifier',
+                                     StructType([StructField('operation', StringType()),
+                                                 StructField('newValue',
+                                                             StructType([StructField('taxIdType', StringType()),
+                                                                         StructField('taxId', StringType())])),
+                                                 StructField('oldValue', NullType())])),
+                         StructField('contractBranchCode',
+                                     StructType([StructField('operation', StringType()),
+                                                 StructField('newValue', StringType()),
+                                                 StructField('oldValue', NullType())])),
+                         StructField('contractCountry',
+                                     StructType([StructField('operation', StringType()),
+                                                 StructField('newValue', StringType()),
+                                                 StructField('oldValue', NullType())]))])
+
+    return spark.read.format("json").schema(schema).load("test_data/results/contract_df.json")
+
+def test_get_config():
+    conf_local = get_config("LOCAL")
+    conf_qa = get_config("QA")
+
+    assert conf_local["kafka.topic"] == "sbdl_kafka_cloud"
+    assert conf_qa["hive.database"] == "sbdl_db_qa"
+
+def test_read_accounts(spark):
+    accounts_df = DataLoader.read_accounts(spark,"LOCAL",False,None)
+    assert accounts_df.count()==8
+
+def test_read_parties(spark, expected_party_rows):
+    actual_party_rows = DataLoader.read_parties(spark, "LOCAL", False, None).collect()
+    assert expected_party_rows == actual_party_rows
+
+def test_get_contract(spark, expected_contract_df):
+    accounts_df = DataLoader.read_accounts(spark, "LOCAL", False, None)
+    actual_contract_df = Transformations.get_contract(accounts_df)
+    assert expected_contract_df.collect()==actual_contract_df.collect()
+
+
 
 
